836b505db2b56a5427957fa41d90613a3f3539b2
Mert Kaptan <mertcagrikaptan@gmail.com>
2023-08-31 17:14:08 +0300

    user model

---
+++ src/models/User.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+}
+
+const UserShema = new mongoose.Schema<User>({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
