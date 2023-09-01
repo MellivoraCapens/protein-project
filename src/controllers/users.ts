51879503e802fdf1be933835cb5f66218b14afaf
Mert Kaptan <mertcagrikaptan@gmail.com>
2023-09-01 18:45:12 +0300

    user CRUD is active

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import User from "../models/User";
+import { ErrorResponse } from "../utils/errorResponse";
 
 export const createUser: RequestHandler = async (req, res, next) => {
   try {
@@ -22,6 +23,75 @@ export const getUsers: RequestHandler = async (req, res, next) => {
     res.status(200).json({
       success: true,
       data: users,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err,
+    });
+  }
+};
+
+export const getUser: RequestHandler = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return next(
+        new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+      );
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err,
+    });
+  }
+};
+
+export const updateUser: RequestHandler = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return next(
+        new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+      );
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err,
+    });
+  }
+};
+
+export const deleteUser: RequestHandler = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return next(
+        new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+      );
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {},
     });
   } catch (err) {
     res.status(400).json({
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/users";
+import {
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/users";
 
 const router = Router();
 
 router.route("/").get(getUsers).post(createUser);
+
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 
 export default router;
