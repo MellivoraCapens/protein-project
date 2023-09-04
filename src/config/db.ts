9b5395a3c0568b49484dfe3058517af972ff5134
Mert Kaptan <mertcagrikaptan@gmail.com>
2023-09-04 12:26:00 +0300

    test folder bug fixed in database

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
+  const conn = await mongoose.connect(
+    `${process.env.MONGO_URI}/protein-project`
+  );
 
   console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
