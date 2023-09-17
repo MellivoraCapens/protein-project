8f99470e579ecf3c4cb5619a8b216c5577a8697d
bariskoklu <30669414+bariskoklu@users.noreply.github.com>
2023-09-17 19:56:05 +0300

    added tailwing, react-router-dom, mui. Also added the routing for the foodspage.tsx, Still WIP on the api connection of it

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import "./App.css";
+import FoodsPage from "./pages/FoodsPage";
+
 
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <div className="font-OpenSans min-h-screen bg-dark text-light">
+      <Router>
+        <Routes>
+          <Route
+            path="/foods"
+            element={<FoodsPage />}
+          />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
---
+++ Client/src/pages/FoodsPage.tsx
@@ -0,0 +1,9 @@
+const FoodsPage = () => {
+    return (
+      <div>
+        <h1 className="text-4xl text-secondary">deneme123</h1>
+      </div>
+    );
+  };
+  
+  export default FoodsPage;
